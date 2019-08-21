6523c89ca38ec9e517ccd96710f7c63043161032
Joan Drago Mateu <45635528+jdrag12@users.noreply.github.com>
2019-08-21 12:06:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- const celeste = document.getElementById('celeste')
+const celeste = document.getElementById('celeste')
 const violeta = document.getElementById('violeta')
 const naranja = document.getElementById('naranja')
 const verde = document.getElementById('verde')
